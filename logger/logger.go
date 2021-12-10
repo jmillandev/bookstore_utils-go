a6f3eb3cf39a2044529779477f70cb88b22a3d1d
Jesús Millán <jgmc3012@gmail.com>
2021-12-10 10:46:20 -0400

    :star: Add LoggerInterface for traking log in databases

--- logger/logger.go
+++ logger/logger.go
@@ -1,18 +1,36 @@
 package logger
 
 import (
+	"fmt"
+	"os"
+	"strings"
+
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-var (
-	log *zap.Logger
+const (
+	envLogLevel  = "LOG_LEVEL"
+	envLogOutput = "LOG_OUTPUT"
 )
+
+var (
+	log logger
+)
+
+type bookstoreLogger interface {
+	Printf(string, ...interface{})
+	Print(...interface{})
+}
+
+type logger struct {
+	log *zap.Logger
+}
 
 func init() {
 	logConfig := zap.Config{
-		OutputPaths: []string{"stdout"},
-		Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
+		OutputPaths: []string{getOutput()},
+		Level:       zap.NewAtomicLevelAt(getLevel()),
 		Encoding:    "json",
 		EncoderConfig: zapcore.EncoderConfig{
 			LevelKey:     "level",
@@ -25,18 +43,58 @@ func init() {
 	}
 	var err error
 
-	if log, err = logConfig.Build(); err != nil {
+	if log.log, err = logConfig.Build(); err != nil {
 		panic(err)
 	}
 }
 
+func getLevel() zapcore.Level {
+	switch strings.ToLower(strings.TrimSpace(os.Getenv(envLogLevel))) {
+	case "debug":
+		return zap.DebugLevel
+	case "info":
+		return zap.InfoLevel
+	case "warn":
+		return zap.WarnLevel
+	case "error":
+		return zap.ErrorLevel
+	default:
+		return zap.InfoLevel
+	}
+}
+
+func getOutput() string {
+	output := strings.ToLower(strings.TrimSpace(os.Getenv(envLogOutput)))
+	if output == "" {
+		return "stdout"
+	}
+	return output
+}
+
+func GetLogger() bookstoreLogger {
+	return log
+}
+
+func (l logger) Printf(v ...interface{}) {
+	Info(fmt.Sprint("%v", v))
+}
+
+func (l logger) Printf(format string, v ...interface{}) {
+	if len(v) == 0 {
+		Info(format)
+		return
+	}
+	msg := fmt.Sprintf(format, v...)
+	Info(msg)
+}
+
 func Info(msg string, tags ...zap.Field) {
-	log.Info(msg, tags...)
-	log.Sync()
+	log.log.Info(msg, tags...)
+	log.log.Sync()
 }
 
 func Error(msg string, err error, tags ...zap.Field) {
 	tags = append(tags, zap.NamedError("error", err))
-	log.Error(msg, tags...)
-	log.Sync()
+	log.log.Error(msg, tags...)
+	log.log.Sync()
 }
